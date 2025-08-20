14204f8bf82e80b47d8f76195efde89e881570b3
Omkar Ranher <omkarranher07@gmail.com>
2025-08-20 20:40:49 +0530

    Update Slip11a.py
    with output of this program,

--- Slip11a.py
+++ Slip11a.py
@@ -8,3 +8,11 @@ print(z)
 print("\nElement-wise sum of the said tuples:")
 result=tuple(map(sum,zip(x,y,z)))
 print(result)
+output:
+Original list: 
+(1, 2, 3, 4)
+(3, 5, 3, 1)
+(2, 2, 3, 1)
+
+Element-wise sum of the said tuples:
+(6, 9, 9, 6)
