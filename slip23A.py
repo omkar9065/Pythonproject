19fcab2fc04ddfc0fcad8ad5e2af59077aa3a742
Omkar Ranher <omkarranher07@gmail.com>
2025-09-11 16:48:48 +0530

    Add files via upload
    create a label and change the label font style using tkinter module.

---
+++ slip23A.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+parent=tk.Tk()
+parent.title("Welcome to India")
+my_label=tk.Label(parent,text="Welcome to BJS College",font=("Ink Free Bold",80))
+my_label.grid(column=0,row=0)
+parent.mainloop()
