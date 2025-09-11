b5a4b60abfcfe8bb13a5af48fb02cd96d3d8c3f6
Omkar Ranher <omkarranher07@gmail.com>
2025-09-11 16:44:21 +0530

    Add files via upload
    to create a label and change the label font style

---
+++ slip6B.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+parent=tk.Tk()
+parent.title("Welcome to TYBBA(CA) Students in SMBST College, Sangamner")
+my_label1=tk.Label(parent,text="Hello, Good Morning Everyone!",font=("Arial Bold",50))
+my_label1.grid(column=0,row=0)
+parent.mainloop()
